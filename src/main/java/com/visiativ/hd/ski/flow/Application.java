a0b5e37f3a9eecf64d6bcaff770e4b1bbfa33085
famaridon <famaridon@visiativ.com>
2016-02-23 15:18:05 +0100

    pom.xml

---
+++ src/main/java/com/visiativ/hd/ski/flow/Application.java
@@ -0,0 +1,25 @@
+package com.visiativ.hd.ski.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by visiativ on 23/02/16.
+ */
+@Controller
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
